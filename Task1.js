5b042a87c33cf5bcc7ffafeef962340760911745
DasPriYam714 <priyamdas.aiub@gmail.com>
2024-02-08 16:28:11 +0600

    task 1

---
+++ Task1.js
@@ -0,0 +1,18 @@
+function calculateMoney(amountTickets) {
+    // Check the input pos or neg
+    if (typeof amountTickets !== 'number' || amountTickets <= 0 || !Number.isInteger(amountTickets)) {
+        return " Please provide a positive number as input.";
+    }
+    
+    var totalSold = amountTickets * 120;
+    var watchmanCost = 500;
+    var lunchExpense = 8 * 50;
+    var totalExpense = watchmanCost + lunchExpense;
+    var remainingMoney = totalSold - totalExpense;
+    
+    return remainingMoney;
+}
+
+// Example usage:
+console.log(calculateMoney(10)); // Output: 600
+console.log(calculateMoney(-5)); // Output: Error: Please provide a positive integer as input.
