61da01366cbd163b2667e473209706e21111f8e4
DasPriYam714 <priyamdas.aiub@gmail.com>
2024-02-08 17:10:04 +0600

    Task4

---
+++ Task4.js
@@ -0,0 +1,23 @@
+function password(userDetails) {
+
+    if (!userDetails || typeof userDetails !== 'object' ||
+        !userDetails.name || !userDetails.birthYear || !userDetails.siteName ||
+        typeof userDetails.birthYear !== 'number' || userDetails.birthYear.toString().length !== 4) {
+        return "Provided details is invalid";
+    }
+    
+    
+    var providedPassword = userDetails.siteName + "#" + userDetails.name + "@" + userDetails.birthYear;
+    
+    return providedPassword;
+}
+
+// Example usage:
+var userDetails = { name: "kolimuddin", birthYear: 1999, siteName: "google" };
+console.log(password(userDetails)); // Output: Google#kolimuddin@1999
+
+var userDetailsMissing = { name: "kolimuddin", siteName: "google" };
+console.log(password(userDetailsMissing)); // Output: invalid
+
+var userDetailsInvalidYear = { name: "kolimuddin", birthYear: 99, siteName: "google" };
+console.log(password(userDetailsInvalidYear)); // Output: invalid
